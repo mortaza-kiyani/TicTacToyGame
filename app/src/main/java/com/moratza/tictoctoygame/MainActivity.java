80785d020cb9aec0267d1cff188c41d507c3ab68
mortazakiyani <36876384+mortazakiyani@users.noreply.github.com>
2020-10-06 18:57:12 +0330

    first build

---
+++ app/src/main/java/com/moratza/tictoctoygame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.moratza.tictoctoygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
